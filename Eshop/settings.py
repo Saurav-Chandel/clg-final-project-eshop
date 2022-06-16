9896b9e0cab5af9c27f9a4019a1e72763daaf6c2
chandel-saurav <chandelsaurav817@gmail.com>
2022-06-16 17:16:22 +0530

    'l'

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -14,7 +14,7 @@ import os
 import datetime
 import os
 from pathlib import Path
-from tkinter import TRUE
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

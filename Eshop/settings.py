697a6702a5820cf261c622e2443f823b064b738b
chandel-saurav <chandelsaurav817@gmail.com>
2022-06-16 17:15:27 +0530

    'a'

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -14,6 +14,7 @@ import os
 import datetime
 import os
 from pathlib import Path
+from tkinter import TRUE
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,8 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^_g%33qd(g8bjc+*40&uh(ptgkb$&-*+0!i3$lu7xj1u166cbb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = TRUE
 ALLOWED_HOSTS = ["*"]
 
 

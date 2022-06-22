7ee79ba0e5997a67fb27230b57c0c38e4877164a
chandel-saurav <chandelsaurav817@gmail.com>
2022-06-22 11:31:12 +0530

    's'

--- store/models/customer.py
+++ store/models/customer.py
@@ -12,63 +12,62 @@ from django.utils.timezone import now
 # Create your models here.
 
 
-# class AppUserManager(UserManager):
-#     def get_by_natural_key(self, username):
-#         return self.get(email__iexact=username)
+class AppUserManager(UserManager):
+    def get_by_natural_key(self, username):
+        return self.get(email__iexact=username)
 
-#     def create_user(self, email, password=None, **extra_fields):
-#         extra_fields.setdefault("is_staff", False)
-#         extra_fields.setdefault("is_superuser", False)
-#         return self._create_user(email, password, **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
 
-#     def _create_user(self, email, password, **extra_fields):
-#         email = self.normalize_email(email)
-#         user = self.model(email=email, **extra_fields)
-#         user.set_password(password)
-#         user.save()
-#         return user
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
-#     def create_superuser(self, email, password=None, **extra_fields):
-#         """
-#         Create a Super Admin. Not to be used by any API. Only used for django-admin command.
-#         :param email:
-#         :param password:
-#         :param extra_fields:
-#         :return:
-#         """
-#         extra_fields.setdefault("is_staff", True)
-#         extra_fields.setdefault("is_superuser", True)
-#         extra_fields.setdefault('is_active', True)
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create a Super Admin. Not to be used by any API. Only used for django-admin command.
+        :param email:
+        :param password:
+        :param extra_fields:
+        :return:
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault('is_active', True)
 
-#         user = self._create_user(email, password, **extra_fields)
-#         return user
+        user = self._create_user(email, password, **extra_fields)
+        return user
 
 
-# class User(AbstractUser):
-#     first_name = models.CharField(
-#         max_length=200, default=None, null=True, blank=True
-#     )
-#     email=models.EmailField(unique=True,null=False)
-#     phone_no = models.CharField(
-#         max_length=20, default=None, null=True, blank=True
-#     )
-#     email_verified = models.BooleanField(default=False)
-#     email_otp = models.CharField(max_length=10, null=True, blank=True)
+class User(AbstractUser):
+    first_name = models.CharField(
+        max_length=200, default=None, null=True, blank=True
+    )
+    email=models.EmailField(unique=True,null=False)
+    phone_no = models.CharField(
+        max_length=20, default=None, null=True, blank=True
+    )
+    email_verified = models.BooleanField(default=False)
+    email_otp = models.CharField(max_length=10, null=True, blank=True)
 
-#     is_deleted = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
 
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-#     USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
 
-#     REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = ['username']
     
-#     manager = AppUserManager()
+    manager = AppUserManager()
 
-#     def __str__(self):
-#         return self.email
-  
+    def __str__(self):
+        return self.email
 
 
 class Customer(models.Model):

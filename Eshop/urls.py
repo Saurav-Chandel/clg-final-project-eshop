65bd7bd70613447ee3e2d854f09d60da9a40cfa7
chandel-saurav <chandelsaurav817@gmail.com>
2022-06-22 12:31:35 +0530

    's'

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django.urls import path  , include
+from django.urls import path  , include ,re_path
 from django.conf.urls.static import static
-from . import settings
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , include('store.urls'))
+    path('' , include('store.urls')),
+    re_path(r'^media_root/(?P<path>.*)$' ,serve,{'document_root':settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$' ,serve,{'document_root':settings.STATIC_ROOT}),
 ] 
 if not settings:
     urlpatterns += [] + static(
